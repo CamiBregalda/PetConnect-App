fb27f84e3c913387af751cbe19bea0de72887c0a
Camila <camilazbregalda@gmail.com>
2025-06-08 20:47:03 -0300

    Corrigindo id de evento

--- backend/src/models/Evento.ts
+++ backend/src/models/Evento.ts
@@ -1,8 +1,9 @@
 import mongoose, { Schema, Document } from 'mongoose';
 import { Endereco, EnderecoSchema } from "./Endereco";
 
-export interface EventoAttributes extends Document {
-  titulo: string;
+
+export interface EventoAttributes {
+    titulo: string;
   descricao: string;
   objetivo: string;
   dataInicio: string;
@@ -10,22 +11,33 @@ export interface EventoAttributes extends Document {
   endereco: Endereco;
   idAbrigo: mongoose.Schema.Types.ObjectId;
   image?: Buffer;
-  imagemUrl?: string;
 }
 
+const EventoSchema = new Schema<EventoAttributes>(
+    {
+      titulo:     { type: String, required: true },
+      descricao:  { type: String, required: true },
+      objetivo:   { type: String, required: true },
+      dataInicio: { type: Date,   required: true },
+      dataFim:    { type: Date,   required: true },
+      endereco:   { type: EnderecoSchema, required: true },
+      idAbrigo:   { type: mongoose.Schema.Types.ObjectId, ref: 'Abrigo', required: true },
+      image:      { type: Buffer, required: false },
+      ativo:      { type: Boolean, default: true },
+    },
+    {
+      collection: "eventos",
+      timestamps: true,
+      toJSON: {
+        virtuals: true,
+        transform: (_, ret) => {
+          ret.id = ret._id;
+          delete ret._id;
+          delete ret.__v;
+        }
+      }
+    }
+);
 
-const eventoSchema = new mongoose.Schema({
-  titulo:     { type: String, required: true },
-  descricao:  { type: String, required: true },
-  objetivo:   { type: String, required: true },
-  dataInicio: { type: Date,   required: true },
-  dataFim:    { type: Date,   required: true },
-  endereco:   { type: EnderecoSchema, required: true },
-  idAbrigo:   { type: mongoose.Schema.Types.ObjectId, ref: 'Abrigo', required: true },
-  image:      { type: Buffer, required: false },
-  ativo:      { type: Boolean, default: true },
-}, {
-  timestamps: true
-});
-
-export default mongoose.model<EventoAttributes>('Evento', eventoSchema);
+const EventoModel = mongoose.model<EventoAttributes>('Evento', EventoSchema);
+export default EventoModel;
